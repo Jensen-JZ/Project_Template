2e42a2531698d3314d4ab3965b0f9482dfaab8df
Jensen-JZ <jensen.acm@gmail.com>
2025-05-03 13:03:26 +0000

    Update Checkpoint Management...

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -26,17 +26,66 @@ class CheckpointManager:
         self.module_dict = kwargs
         self.multi_gpu = multi_gpu
 
-    def register(self, *kwargs):
-        
-        pass
+
+    def register(self, **kwargs):
+        """
+        Registers additional modules for checkpointing after initialization.
+
+        Args:
+            **kwargs: Keyword arguments mapping names to PyTorch modules.
+        """
+        self.module_dict.update(kwargs)
 
 
     def save(self, step):
-        pass
+        """
+        Saves the state_dict of all registered modules to disk.
+
+        Args:
+            step (int): Training step or epoch number used for naming the ckpt file.
+        """
+        
+        ckpt_path = self.ckpt_path_template.format(step)
+        print(f'Saving checkpoint to {ckpt_path}...')
+        state_dict = {}
+        for name, module in self.module_dict.items():
+            if self.multi_gpu:
+                state_dict[name] = module.module.state_dict()
+            else:
+                state_dict[name] = module.state_dict()
+        torch.save(state_dict, ckpt_path)
+        print(f'Checkpoint saved to {ckpt_path}.')
+
 
     def load(self, step):
-        pass
+        """
+        Loads checkpoint from a given step number.
 
-    def load_from_path(self, file_path):
-        pass
+        Args:
+            step (int): Training step or epoch number used to format the ckpt file name.
+        """
+        
+        ckpt_path = self.ckpt_path_template.format(step)
+        print(f'Loading checkpoint from {ckpt_path}...')
+        self.load_from_path(ckpt_path)
+        print(f'Checkpoint loaded from {ckpt_path}.')
 
+
+    def load_from_path(self, ckpt_path):
+        """
+        Loads ckpt from a full path.
+
+        Args:
+            ckpt_path (str): Full path to the ckpt file.
+        """
+        
+        assert os.path.exists(ckpt_path), f'Checkpoint file {ckpt_path} does not exist.'
+        if torch.cuda.is_available():
+            module_dict = torch.load(ckpt_path)
+        else:
+            module_dict = torch.load(ckpt_path, map_location=torch.device('cpu'))
+        for name, module in self.module_dict.items():
+            if self.multi_gpu:
+                module.module.load_state_dict(module_dict[name])
+            else:
+                module.load_state_dict(module_dict[name])
